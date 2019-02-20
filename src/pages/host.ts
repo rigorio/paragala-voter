100d2ec640823d8c7a16a6d5577ba0c905cbd6af
rigo_sarmiento <rigo.sarmiento@pm..me>
2019-02-20 23:04:43 +0800

    update version and host

--- src/pages/host.ts
+++ src/pages/host.ts
@@ -1,4 +1,4 @@
 export class Host {
-  // public static host = "https://shrouded-caverns-92003.herokuapp.com";
-  public static host = "http://localhost:8080";
+  public static host = "https://shrouded-caverns-92003.herokuapp.com";
+  // public static host = "http://localhost:8080";
 }
